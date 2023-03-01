0a9ce5fce10d86dabedfb8b90a6503752690b3fb
Arsene Rurangiza <arurangi@mi-r2-p3.s19.be>
2023-03-01 17:01:55 +0100

    imprv: partially handles cat | ls

--- srcs/execution.c
+++ srcs/execution.c
@@ -6,7 +6,7 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/11 20:01:10 by Arsene            #+#    #+#             */
-/*   Updated: 2023/03/01 15:45:12 by arurangi         ###   ########.fr       */
+/*   Updated: 2023/03/01 17:01:09 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@ void	execute(t_token *token, int nbr_of_pipes)
 {
 	int	index = 0, pipends[2], prevpipe = 69, cmd_type;
 
-	printf("Executing...\n");
 	while (index < nbr_of_pipes)
 	{
         cmd_type = get_cmd_type(nbr_of_pipes, index);
@@ -55,7 +54,6 @@ void	single_child(t_token *token)
 {
 	int	error_code;
 
-	printf("-> single_child\n");
 	if (token->infile != -1)
 		redirect_in(token);
 	if (token->outfile != -1)
@@ -69,7 +67,6 @@ void	last_child(t_token *token, int prevpipe)
 {
 	int error_code;
 
-	printf("-> last_child\n");
 	if (token->infile != -1)
 		redirect_in(token);
 	else
@@ -85,22 +82,24 @@ void	last_child(t_token *token, int prevpipe)
 void	middle_child(t_token *token, int index, int prevpipe, int *pipends)
 {
 	int error_code;
-	
-	//printf("-> middle_child\n");
+
 	close(pipends[READ]);
 	if (token->infile != -1)
 		redirect_in(token);
 	else if (index > 0)
 		dup2(prevpipe, STDIN_FILENO);
 	close(prevpipe);
-	//printf("infile: %i, outfile: %i\n", token->infile, token->outfile);
+
+	if (ft_strncmp(token->cmd[0], "cat", 3) == 0 && token->cmd[1] == NULL)
+		exit(0);
+	
 	if (token->outfile != -1)
 		redirect_out(token);
 	else
 	{
-		//printf("---- got here\n");
 		//printf("error_code = %i\n", error_code);
-		error_code = dup2(pipends[WRITE], STDOUT_FILENO);
+		if (dup2(pipends[WRITE], STDOUT_FILENO) == -1)
+			printf("Error with DUP2()\n");
 	}
 	close(pipends[WRITE]);
 	error_code = execve(token->cmd_path, token->cmd, token->envp);
