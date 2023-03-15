5a9e32b9f2b4b443097db2a3c8ddb1031be7ab9f
Arsene Rurangiza <arurangi@shi-r10-p4.s19.be>
2023-03-15 11:48:13 +0100

    imprv: expander handles 0 next to each other

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 08:52:49 by Arsene            #+#    #+#             */
-/*   Updated: 2023/03/15 11:07:51 by arurangi         ###   ########.fr       */
+/*   Updated: 2023/03/15 11:47:14 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -60,13 +60,20 @@ char *expand_variable(char *buffer)
 	// Find start
 	start = ft_strchr_mod(buffer, '$') + 1;
 	if (!buffer[start] || (buffer[start] == '\"') || buffer[start] == '?')
-		return(buffer);
+	{
+		expanded = ft_itoa(g_tools.exit_code);
+		end = start + 1;
+		tmp = ft_strjoin_trio(ft_substr(buffer, 0, start - 1), expanded, ft_strdup(buffer + end));
+		free(buffer);
+		return(tmp);
+	}
 	// Find end
 	end = start;
-	while (buffer[end] && !ft_isspace(buffer[end]) && buffer[end] != '\"')
+	while (buffer[end] && !ft_isspace(buffer[end]) && buffer[end] != '\"' && buffer[end] != '$')
 		end++;
 	// Isolate the variable
 	variable = ft_substr(buffer, start, end - start);
+	//printf("%s\n", variable);
 	// Check if variable exists in envp
 	expanded = get_envp_variable(variable);
 	if (expanded)
