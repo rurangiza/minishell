ea78c1204f4f5df23d08d6c9798d1779bab4bbc3
Arsene Rurangiza <arurangi@shi-r10-p4.s19.be>
2023-03-15 11:18:18 +0100

    imprv: echo(): echo 0 works

--- built-ins/echo.c
+++ built-ins/echo.c
@@ -6,7 +6,7 @@
 /*   By: arurangi <arurangi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 13:49:57 by arurangi          #+#    #+#             */
-/*   Updated: 2023/03/15 11:04:48 by arurangi         ###   ########.fr       */
+/*   Updated: 2023/03/15 11:16:59 by arurangi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,13 +41,16 @@ void	echo(t_token *token)
 	while (token->cmd[index])
 	{
 		if (ft_strncmp("$?", token->cmd[index], 2) == 0)
-			printf("%i bra", g_tools.exit_code);
+			write(STDOUT_FILENO, ft_itoa(g_tools.exit_code), ft_strlen(ft_itoa(g_tools.exit_code)));
 		else
 			write(STDOUT_FILENO, token->cmd[index], ft_strlen(token->cmd[index]));
-		if (index > 0 && index < ft_strlen(token->cmd[index]) - 1)
+		if (index > 0 && index < ft_strlen(token->cmd[index]))
 			write(STDOUT_FILENO, " ", 1);
 		index++;
 	}
 	if (newline_mode == 1)
+	{
+		//printf("NEWLINE MODE\n");
 		write(STDOUT_FILENO, "\n", 1);
+	}
 }
